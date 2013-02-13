879255034c3ea129fb31d52e6d5409f27ef4e216
chaos <yume_@live.com>
2013-02-13 14:15:29 +0300

    spree ru

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spro::Application.config.secret_token = 'ac0d84368f719162de02982fe3a393c9aa723b35ec3bfa15f11e81b5efef84b0a68c0979067a4a01426f474c78914308a8e5f73ec6adae4e56f15da0afc63647'
